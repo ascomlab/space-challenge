76dff64b296bd96462bd83e5f8f0a31f49dd26a2
Llam4u <thezeroyou@gmail.com>
2023-11-29 14:40:07 +0700

    style: update overall styling

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,4 +1,4 @@
-var endDate = "09/23/"; 
+var endDate = "12/03/2023";  // format mm/dd/yyyy
 
 (function () {
   const second = 1000,
@@ -6,21 +6,10 @@ var endDate = "09/23/";
         hour = minute * 60,
         day = hour * 24;
 
-  //I'm adding this section so I don't have to keep updating this pen every year :-)
-  //remove this if you don't need it
-  let today = new Date(),
-      dd = String(today.getDate()).padStart(2, "0"),
-      mm = String(today.getMonth() + 1).padStart(2, "0"),
-      yyyy = today.getFullYear(),
-      nextYear = yyyy + 1,
-      dayMonth = endDate,
-      birthday = dayMonth + yyyy;
+  var dateArray = endDate.split("/");
 
-  today = mm + "/" + dd + "/" + yyyy;
-  if (today > birthday) {
-    birthday = dayMonth + nextYear;
-  }
-  //end
+  // Create a new Date object (months are zero-based)
+  var birthday = new Date(dateArray[2], dateArray[0] - 1, dateArray[1]);
 
   const countDown = new Date(birthday).getTime(),
       x = setInterval(function() {
