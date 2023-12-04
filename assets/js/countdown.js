86b7fb58630f1be572f403dd768bc6c0f5f35c19
Llam4u <thezeroyou@gmail.com>
2023-12-04 10:58:38 +0700

    refactor: render 2 digit number on count down
    Note:
    - change complete text to 0 count down

--- assets/js/countdown.js
+++ assets/js/countdown.js
@@ -1,4 +1,4 @@
-var endDate = "12/03/2023";  // format mm/dd/yyyy
+var endDate = "12/30/2023";  // format mm/dd/yyyy
 
 (function () {
   const second = 1000,
@@ -17,16 +17,20 @@ var endDate = "12/03/2023";  // format mm/dd/yyyy
         const now = new Date().getTime(),
               distance = countDown - now;
 
-        document.getElementById("days").innerText = Math.floor(distance / (day)),
-          document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour)),
-          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
-          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+        document.getElementById("days").innerText = String(Math.floor(distance / (day))).padStart(2, '0'),
+          document.getElementById("hours").innerText = String(Math.floor((distance % (day)) / (hour))).padStart(2, '0'),
+          document.getElementById("minutes").innerText = String(Math.floor((distance % (hour)) / (minute))).padStart(2, '0'),
+          document.getElementById("seconds").innerText = String(Math.floor((distance % (minute)) / second)).padStart(2, '0');
 
         //do something later when date is reached
         if (distance < 0) {
           // document.getElementById("headline").innerText = "It's my birthday!";
-          document.getElementById("countdown").style.display = "none";
-          document.getElementById("content").style.display = "block";
+          // document.getElementById("countdown").style.display = "none";
+          // document.getElementById("content").style.display = "block";
+          document.getElementById("days").innerText = "00";
+          document.getElementById("hours").innerText = "00";
+          document.getElementById("minutes").innerText = "00";
+          document.getElementById("seconds").innerText = "00";
           clearInterval(x);
         }
         //seconds
